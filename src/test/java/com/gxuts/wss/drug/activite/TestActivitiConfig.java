45ac75a666609ccc3c930cccf8cfe7237136080c
nf0830 <nf0830@OWEN-PC.Everich.local>
2015-01-19 19:34:58 +0800

    7

--- src/test/java/com/gxuts/wss/drug/activite/TestActivitiConfig.java
+++ src/test/java/com/gxuts/wss/drug/activite/TestActivitiConfig.java
@@ -1,8 +1,13 @@
 package com.gxuts.wss.drug.activite;
 
+import java.util.List;
+
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.task.Task;
 import org.activiti.spring.ProcessEngineFactoryBean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +24,8 @@ public class TestActivitiConfig {
 	private RuntimeService runtimeService;
 	@Autowired
 	private RepositoryService repositoryService;
-
+	@Autowired
+	private TaskService taskService;
 	@Test
 	public void testGetEngin() {
 		System.out.println(repositoryService);
@@ -27,11 +33,35 @@ public class TestActivitiConfig {
 
 	@Test
 	public void testExport() {
-		DeploymentBuilder builder = repositoryService.createDeployment();
-		builder.addClasspathResource("process/export.bpmn");
-		builder.deploy();
+		Deployment deploy = repositoryService.createDeployment() //
+							.addClasspathResource("process/export.bpmn")//
+							.deploy();
 		// select * from ACT_GE_PROPERTY;这时这个表中会多条数据
 		runtimeService.startProcessInstanceByKey("export");// 启动流程，ID必须与你配置的一致
 		System.out.println("over......");
 	}
+    //查个人任务列表
+	@Test
+	public void testDeploy() {
+		List<Task> taskList=taskService.createTaskQuery().taskAssignee("李丽").list();
+//		for (int i = 0; i < taskList.size(); i++) {
+			Task task=taskList.get(1);
+			System.out.println(task.getAssignee());
+			System.out.println(task.getId());
+			System.out.println("节点名称："+task.getName());
+			System.out.println(task.getOwner());
+			System.out.println(task.getDescription());
+			System.out.println(task.getParentTaskId());
+			System.out.println(task.getProcessInstanceId());
+			System.out.println("优先级："+task.getPriority());
+			System.out.println(task.getCategory());
+			System.out.println(task.getExecutionId());
+			System.out.println(task.getProcessDefinitionId());
+			System.out.println("节点ID："+task.getTaskDefinitionKey());
+			System.out.println(task.getTenantId());
+			System.out.println(task.getCreateTime());
+			System.out.println(task.getDelegationState());
+			System.out.println(task.getDueDate());
+		}
+//	}
 }
