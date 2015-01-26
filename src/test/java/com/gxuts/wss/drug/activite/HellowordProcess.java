3e2c95812d0535a82e7afbce01c0c7edcdc3c4ce
nf0830 <nf0830@OWEN-PC.Everich.local>
2015-01-26 09:08:10 +0800

    7

--- src/test/java/com/gxuts/wss/drug/activite/HellowordProcess.java
+++ src/test/java/com/gxuts/wss/drug/activite/HellowordProcess.java
@@ -10,7 +10,10 @@ import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.history.HistoricTaskInstance;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Attachment;
+import org.activiti.engine.task.Comment;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -42,7 +45,7 @@ public class HellowordProcess {
 		System.out.println(deploy.getCategory());
 		System.out.println(deploy.getTenantId());
 	}
-	//10001-启动30001
+	//52501
 	@Test
 	public void testStart(){
 		ProcessInstance processInstance=runtimeService.startProcessInstanceByKey("leave");
@@ -75,6 +78,10 @@ public class HellowordProcess {
 			System.out.println("----------");  
 		}
 	}
+	/*张三
+	52504
+	提交申请
+	52501*/
 	@Test
 	public void testQueryTask(){
 		String assignee="张三";
@@ -83,6 +90,7 @@ public class HellowordProcess {
 			System.out.println(t.getAssignee());
 			System.out.println(t.getId());
 			System.out.println(t.getName());
+			System.out.println(t.getProcessDefinitionId());
 			System.out.println(""+t.getProcessInstanceId());
 			System.out.println("----------");  
 		}
@@ -90,12 +98,12 @@ public class HellowordProcess {
 	//办理10004
 	@Test
 	public void testComplete(){
-		String taskId="35003";
-		String processInstanceId="30001";
+		String taskId="52504";
+		String processInstanceId="52501";
 		String type = null;
 		Map<String, Object> variables=new HashMap<String, Object>();
 		variables.put("message", "同意");
-		taskService.addComment(taskId, processInstanceId, type, "很好");
+		taskService.addComment(taskId, processInstanceId, type, "总监意见：真好");
 		taskService.complete(taskId, variables);
 		System.out.println("完成");
 		
@@ -115,6 +123,28 @@ public class HellowordProcess {
 			System.out.println(h.getName());
 		}
 	}
-	
+	//获取审批意见
+	@Test
+	public void testComment(){
+		String taskId="45004";
+		String processInstanceId="45001";
+		List<Comment> comment =taskService.getProcessInstanceComments(processInstanceId);
+//		List<Comment> comment=taskService.getTaskComments(taskId);
+		for(Comment a:comment){
+			System.out.print(a.getFullMessage());
+			System.out.print(a.getUserId());
+			System.out.print(a.getType());
+			System.out.print(a.getTime());
+			System.out.println();
+		}
+	}
+	@Test
+	public void  testImg(){
+		ProcessInstance processInstance=runtimeService.startProcessInstanceByKey("leave");
+		ProcessDefinition pd = repositoryService.createProcessDefinitionQuery()//创建流程定义查询对象，对应表act_re_procdef 
+				.processDefinitionId(processInstance.getProcessDefinitionId())//使用流程定义ID查询
+				.singleResult();
+		System.out.println(pd.getDiagramResourceName());
+	}
 	
 }
