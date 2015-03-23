931d0afb6661e61a02c20a7fd8d980705c203c5e
nf0830 <nf0830@OWEN-PC.Everich.local>
2015-03-23 18:29:16 +0800

    11

--- src/main/java/com/gxuts/wss/dms/base/CommentClassTest.java
+++ src/main/java/com/gxuts/wss/dms/base/CommentClassTest.java
@@ -1,11 +1,5 @@
 package com.gxuts.wss.dms.base;
 
-import org.activiti.engine.TaskService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-
-import com.gxuts.wss.dms.service.hr.UserService;
 public class CommentClassTest<F> {
 	public F test(F f){
 		return f;
