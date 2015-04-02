cc30bf253c2c280838cf05dfd7ea98e9055b057a
nf0830 <nf0830@OWEN-PC.Everich.local>
2015-04-02 17:26:26 +0800

    log

---
+++ src/main/java/com/gxuts/wss/dms/service/sys/ControllerLogService.java
@@ -0,0 +1,20 @@
+package com.gxuts.wss.dms.service.sys;
+
+import com.gxuts.wss.dms.entity.sys.ControllerLog;
+
+
+/**
+ * ControllerLogService
+ * 
+ * http://git.oschina.net/sphsyv/sypro
+ *
+ * 由代码生成器生成
+ *
+ * @author 孙宇
+ *
+ */
+public interface ControllerLogService  {
+
+	void save(ControllerLog controllerLog);
+
+}
---
+++ src/main/java/com/gxuts/wss/dms/service/sys/impl/ControllerLogServiceImpl.java
@@ -0,0 +1,30 @@
+package com.gxuts.wss.dms.service.sys.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.gxuts.wss.dms.entity.sys.ControllerLog;
+import com.gxuts.wss.dms.service.sys.ControllerLogService;
+
+
+/**
+ * ControllerLogServiceImpl
+ * 
+ * http://git.oschina.net/sphsyv/sypro
+ *
+ * 由代码生成器生成
+ *
+ * @author 孙宇
+ *
+ */
+@Service("controllerLogService")
+public class ControllerLogServiceImpl  implements ControllerLogService {
+
+	@Override
+	public void save(ControllerLog controllerLog) {
+		
+	}
+
+
+}
